867d458cea1ec32b730eb825fc68c752f6f109eb
althaafka <althaafka004@gmail.com>
2023-10-16 00:51:26 +0700

    add bot interface

--- src/Bot.java
+++ src/Bot.java
@@ -1,6 +1,3 @@
-public class Bot {
-    public int[] move() {
-        // create random move
-        return new int[]{(int) (Math.random()*8), (int) (Math.random()*8)};
-    }
+interface Bot {
+    public abstract int[] move(String[][] gameState);
 }
